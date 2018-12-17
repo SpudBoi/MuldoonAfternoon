b36eb1ac1af324f14b63d77f33b8dda96ca78286
Conor Muldoon <43060779+SpudBoi@users.noreply.github.com>
2018-12-16 23:33:06 -0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <h1>About me </h1>
+</head>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Muldoon Afternoon</title>
+</head>
+
+<body>
+	Hello, welcome.
+</body>
+
+</html>
